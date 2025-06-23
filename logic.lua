86d47cc1db9352f86ac4229cfbc533ac4aa8f499
Odysseas Chryssos <ody.chryssos@gmail.com>
2025-06-23 03:12:23 +0300

    Update logic.lua

--- logic.lua
+++ logic.lua
@@ -1,3 +1,4 @@
 local browser = window.browser
+local browserName = window.browserName
 local br = get('browser', false)
-br.set_contents("Browser: " .. browser) 
+br.set_contents("Browser: " .. browser .. " on " ... browserName) 
